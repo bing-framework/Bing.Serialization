3c5b665b2747e2e46a7814d5a0d20eb5ad481962
jian玄冰 <jianxuanhuo1@126.com>
2023-06-06 16:51:10 +0800

    fix: 实现 Newtonsoft Json 序列化器

--- src/Bing.Serialization.NewtonsoftJson/Bing/Serialization/NewtonsoftJsonSerializer.cs
+++ src/Bing.Serialization.NewtonsoftJson/Bing/Serialization/NewtonsoftJsonSerializer.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Threading.Tasks;
-using Bing.Serialization.Json.Newtonsoft;
-using Bing.Serialization.Newtonsoft;
-using Newtonsoft.Json;
-using Newtonsoft.Json.Linq;
+﻿using Bing.Serialization.Newtonsoft;
 
 namespace Bing.Serialization
 {
@@ -86,75 +81,39 @@ namespace Bing.Serialization
         public object FromStream(Type type, Stream stream) => NewtonsoftJsonHelper.FromStream(type, stream);
 
         /// <inheritdoc />
-        public Task<MemoryStream> ToStreamAsync<TValue>(TValue value, CancellationToken cancellationToken = default)
-        {
-            throw new NotImplementedException();
-        }
+        public Task<MemoryStream> ToStreamAsync<TValue>(TValue value, CancellationToken cancellationToken = default) => NewtonsoftJsonHelper.ToStreamAsync(value, _settings, cancellationToken: cancellationToken);
 
         /// <inheritdoc />
-        public Task<MemoryStream> ToStreamAsync(Type type, object value, CancellationToken cancellationToken = default)
-        {
-            throw new NotImplementedException();
-        }
+        public Task<MemoryStream> ToStreamAsync(Type type, object value, CancellationToken cancellationToken = default) => NewtonsoftJsonHelper.ToStreamAsync(value, _settings, cancellationToken: cancellationToken);
 
         /// <inheritdoc />
-        public Task<TValue> FromStreamAsync<TValue>(Stream stream, CancellationToken cancellationToken = default)
-        {
-            throw new NotImplementedException();
-        }
+        public Task<TValue> FromStreamAsync<TValue>(Stream stream, CancellationToken cancellationToken = default) => NewtonsoftJsonHelper.FromStreamAsync<TValue>(stream, _settings, cancellationToken: cancellationToken);
 
         /// <inheritdoc />
-        public Task<object> FromStreamAsync(Type type, Stream stream, CancellationToken cancellationToken = default)
-        {
-            throw new NotImplementedException();
-        }
+        public Task<object> FromStreamAsync(Type type, Stream stream, CancellationToken cancellationToken = default) => NewtonsoftJsonHelper.FromStreamAsync(type, stream, _settings, cancellationToken: cancellationToken);
 
         /// <inheritdoc />
-        public TValue FromText<TValue>(string text)
-        {
-            throw new NotImplementedException();
-        }
+        public TValue FromText<TValue>(string text) => NewtonsoftJsonHelper.FromJson<TValue>(text, _settings);
 
         /// <inheritdoc />
-        public object FromText(Type type, string text)
-        {
-            throw new NotImplementedException();
-        }
+        public object FromText(Type type, string text) => NewtonsoftJsonHelper.FromJson(type, text, _settings);
 
         /// <inheritdoc />
-        public string ToText<TValue>(TValue value)
-        {
-            throw new NotImplementedException();
-        }
+        public string ToText<TValue>(TValue value) => NewtonsoftJsonHelper.ToJson(value, _settings);
 
         /// <inheritdoc />
-        public string ToText(Type type, object value)
-        {
-            throw new NotImplementedException();
-        }
+        public string ToText(Type type, object value) => NewtonsoftJsonHelper.ToJson(value, _settings);
 
         /// <inheritdoc />
-        public TValue FromJson<TValue>(string json)
-        {
-            throw new NotImplementedException();
-        }
+        public TValue FromJson<TValue>(string json) => NewtonsoftJsonHelper.FromJson<TValue>(json, _settings);
 
         /// <inheritdoc />
-        public object FromJson(Type type, string json)
-        {
-            throw new NotImplementedException();
-        }
+        public object FromJson(Type type, string json) => NewtonsoftJsonHelper.FromJson(type, json, _settings);
 
         /// <inheritdoc />
-        public string ToJson<TValue>(TValue value)
-        {
-            throw new NotImplementedException();
-        }
+        public string ToJson<TValue>(TValue value) => NewtonsoftJsonHelper.ToJson(value, _settings);
 
         /// <inheritdoc />
-        public string ToJson(Type type, object value)
-        {
-            throw new NotImplementedException();
-        }
+        public string ToJson(Type type, object value) => NewtonsoftJsonHelper.ToJson(value, _settings);
     }
 }
