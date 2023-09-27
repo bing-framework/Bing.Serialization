47385b5608856d77b8139392d5782fdcafbd169c
jian玄冰 <jianxuanhuo1@126.com>
2023-09-27 14:23:43 +0800

    feat: 新增 decimal 类型的Json转换器

---
+++ src/Bing.Serialization.SystemTextJson/Bing/Serialization/SystemTextJson/JsonConverters/DecimalJsonConverter.cs
@@ -0,0 +1,19 @@
+﻿using System.Globalization;
+using System.Text.Json.Serialization;
+
+namespace Bing.Serialization.SystemTextJson.JsonConverters;
+
+/// <summary>
+/// Decimal数据类型Json转换(用于将字符串类型的数字转化成后端可识别的decimal类型)
+/// </summary>
+public sealed class DecimalJsonConverter : JsonConverter<decimal>
+{
+    /// <inheritdoc />
+    public override decimal Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options) =>
+        reader.TokenType == JsonTokenType.Number
+            ? reader.GetDecimal()
+            : decimal.Parse(reader.GetString()!);
+
+    /// <inheritdoc />
+    public override void Write(Utf8JsonWriter writer, decimal value, JsonSerializerOptions options) => writer.WriteStringValue(value.ToString(CultureInfo.CurrentCulture));
+}
---
+++ src/Bing.Serialization.SystemTextJson/Bing/Serialization/SystemTextJson/JsonConverters/DecimalNullableJsonConverter.cs
@@ -0,0 +1,23 @@
+﻿using System.Globalization;
+using System.Text.Json.Serialization;
+
+namespace Bing.Serialization.SystemTextJson.JsonConverters;
+
+/// <summary>
+/// 可空Decimal数据类型Json转换(用于将字符串类型的数字转化成后端可识别的decimal类型)
+/// </summary>
+public sealed class DecimalNullableJsonConverter : JsonConverter<decimal?>
+{
+    /// <inheritdoc />
+    public override decimal? Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+    {
+        return reader.TokenType == JsonTokenType.Number
+            ? reader.GetDecimal()
+            : string.IsNullOrEmpty(reader.GetString())
+                ? default(decimal?)
+                : decimal.Parse(reader.GetString()!);
+    }
+
+    /// <inheritdoc />
+    public override void Write(Utf8JsonWriter writer, decimal? value, JsonSerializerOptions options) => writer.WriteStringValue(value?.ToString(CultureInfo.CurrentCulture));
+}
