295d02a29656948f9c88070c0fe3ce4f8d60c2a3
jian玄冰 <jianxuanhuo1@126.com>
2023-09-27 18:23:18 +0800

    feat: 新增 DateOnly 类型的Json转换器

---
+++ src/Bing.Serialization.SystemTextJson/Bing/Serialization/SystemTextJson/JsonConverters/Constant.cs
@@ -0,0 +1,22 @@
+﻿namespace Bing.Serialization.SystemTextJson.JsonConverters;
+
+/// <summary>
+/// 常量
+/// </summary>
+internal static class Constant
+{
+    /// <summary>
+    /// 日期格式
+    /// </summary>
+    internal const string DateFormat = "yyyy-MM-dd";
+
+    /// <summary>
+    /// 时间格式
+    /// </summary>
+    internal const string TimeFormat = "HH:mm:ss";
+
+    /// <summary>
+    /// 日期时间格式
+    /// </summary>
+    internal const string DateTimeFormat = $"{DateFormat} {TimeFormat}";
+}
---
+++ src/Bing.Serialization.SystemTextJson/Bing/Serialization/SystemTextJson/JsonConverters/DateOnlyJsonConverter.cs
@@ -0,0 +1,19 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Bing.Serialization.SystemTextJson.JsonConverters;
+
+#if NET6_0_OR_GREATER
+
+/// <summary>
+/// <see cref="DateOnly"/> 类型Json转换(用于将字符串类型的日期转化成后端可识别的 <see cref="DateOnly"/> 类型)
+/// </summary>
+public class DateOnlyJsonConverter : JsonConverter<DateOnly>
+{
+    /// <inheritdoc />
+    public override DateOnly Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options) => DateOnly.Parse(reader.GetString()!);
+
+    /// <inheritdoc />
+    public override void Write(Utf8JsonWriter writer, DateOnly value, JsonSerializerOptions options) => writer.WriteStringValue(value.ToString(Constant.DateFormat));
+}
+
+#endif
