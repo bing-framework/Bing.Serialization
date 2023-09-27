92fff0e923f7691b325665bae3474a5e5fa08c1d
jian玄冰 <jianxuanhuo1@126.com>
2023-09-27 11:24:23 +0800

    feat: 新增 Bool 类型 Json转换器

---
+++ src/Bing.Serialization.SystemTextJson/Bing/Serialization/SystemTextJson/JsonConverters/BoolJsonConverter.cs
@@ -0,0 +1,29 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Bing.Serialization.SystemTextJson.JsonConverters;
+
+/// <summary>
+/// <see cref="bool"/> 类型Json转换(用于将字符串类型 <see langword="true"/> 或 <see langword="false"/> 转化成后端可识别的 <see cref="bool"/> 类型)
+/// </summary>
+/// <example>
+///  <code>
+/// <![CDATA[
+///  builder.Services.AddControllers().AddJsonOptions(c => c.JsonSerializerOptions.Converters.Add(new BoolJsonConverter()));
+///  ]]>
+///  </code>
+/// </example>
+public sealed class BoolJsonConverter : JsonConverter<bool>
+{
+    /// <inheritdoc />
+    public override bool Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options) =>
+        reader.TokenType switch
+        {
+            JsonTokenType.True or JsonTokenType.False => reader.GetBoolean(),
+            JsonTokenType.String => bool.Parse(reader.GetString()!),
+            JsonTokenType.Number => reader.GetDouble() > 0,
+            _ => throw new NotImplementedException($"un processed token type {reader.TokenType}")
+        };
+
+    /// <inheritdoc />
+    public override void Write(Utf8JsonWriter writer, bool value, JsonSerializerOptions options) => writer.WriteBooleanValue(value);
+}
---
+++ src/Bing.Serialization.SystemTextJson/Bing/Serialization/SystemTextJson/JsonConverters/BoolNullableJsonConverter.cs
@@ -0,0 +1,36 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Bing.Serialization.SystemTextJson.JsonConverters;
+
+/// <summary>
+/// 可空 <see cref="bool"/> 类型Json转换(用于将字符串类型 <see langword="true"/> 或 <see langword="false"/> 转化成后端可识别的 <see cref="bool"/> 类型)
+/// </summary>
+/// <example>
+///  <code>
+/// <![CDATA[
+///  builder.Services.AddControllers().AddJsonOptions(c => c.JsonSerializerOptions.Converters.Add(new BoolNullableJsonConverter()));
+///  ]]>
+///  </code>
+/// </example>
+public class BoolNullableJsonConverter : JsonConverter<bool?>
+{
+    /// <inheritdoc />
+    public override bool? Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options) =>
+        reader.TokenType switch
+        {
+            JsonTokenType.True or JsonTokenType.False => reader.GetBoolean(),
+            JsonTokenType.Null => null,
+            JsonTokenType.String => bool.Parse(reader.GetString()!),
+            JsonTokenType.Number => reader.GetDouble() > 0,
+            _ => throw new NotImplementedException($"un processed token type {reader.TokenType}")
+        };
+
+    /// <inheritdoc />
+    public override void Write(Utf8JsonWriter writer, bool? value, JsonSerializerOptions options)
+    {
+        if (value is not null)
+            writer.WriteBooleanValue(value.Value);
+        else
+            writer.WriteNullValue();
+    }
+}
